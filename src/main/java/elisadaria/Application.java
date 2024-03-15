0647b5361e40af9d7c619a98ac3134bdee7e2fcf
elisa-daria <elisa.daria.tomasi@gmail.com>
2024-03-15 10:30:01 +0100

    adds-dependencies

---
+++ src/main/java/elisadaria/Application.java
@@ -0,0 +1,15 @@
+package elisadaria;
+
+import com.github.javafaker.Faker;
+
+import java.util.Locale;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Faker faker=new Faker(Locale.ITALY);
+
+        System.out.println(faker.backToTheFuture().character());
+    }
+}
